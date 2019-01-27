f4879f9abd506f3f72b9f632d7cea280baa83b10
KG102 <46473503+KG102@users.noreply.github.com>
2019-01-27 21:17:57 +0530

    Update Project.java

--- Assignment1/src/Project.java
+++ Assignment1/src/Project.java
@@ -1,15 +1,11 @@
 class Project
 {
 	String name;
-	String startDate;
-    String endDate;
-    String role;
+	String role;
     String[]responsibilities;
-    Project(String name,String startDate,String endDate,String role,String responsibilities[])
+    Project(String name,String role,String responsibilities[])
     {
     	this.name= name;
-    	this.startDate= startDate;
-    	this.endDate= endDate;
     	this.role= role;
     	this.responsibilities= responsibilities;
     }  
